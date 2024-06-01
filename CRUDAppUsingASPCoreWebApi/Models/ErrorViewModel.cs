e3c89f0fadbc4604cd0288ccbaf62754cbbb2015
Urja4157 <urjabajj2013@gmail.com>
2024-06-01 22:41:46 +0545

    Add project files.

---
+++ CRUDAppUsingASPCoreWebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDAppUsingASPCoreWebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CRUDAppUsingASPCoreWebApi/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace CRUDAppUsingASPCoreWebApi.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string studentName { get; set; }
+        public string studentGender { get; set; }
+        private int age { get; set; }
+        private int standard { get; set; }
+        public string fatherName { get; set; }
+
+    }
+}
